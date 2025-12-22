84b47b09c9315ad68aeb77b580909c06d8647b2a
prajot <prajotkulkarni.bgm@gmail.com>
2025-12-22 19:07:33 +0530

    error fix bin file

--- bin/aws-cdk-patterns.ts
+++ bin/aws-cdk-patterns.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { ApiEc2LambdaSnsStack } from '../lib/aws-cdk-patterns-construct';
+import { ApiEc2LambdaSnsStack  } from '../lib/aws-cdk-patterns-stack';
 
 const app = new cdk.App();
-new ApiEc2LambdaSnsStack(app, 'AWS-CDK-Pattern', {
+new ApiEc2LambdaSnsStack (app, 'AWS-CDK-Pattern', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
